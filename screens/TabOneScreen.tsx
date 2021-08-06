5af4cd4d0657f831ba00512f7625ccb0dba4aec3
Samuel T Almeida <samuel.almeida@capgemini.com>
2021-08-06 00:46:06 -0300

    estrutura e listando mapa sem marcações

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,28 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+   /*<View style={styles.container}>
+      <Text style={styles.title}>Portal Cidadão</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    </View>-->*/
+    <MapView
+      style={{
+        flex: 1
+      }}
+      initialRegion={{
+        latitude: -25.412127,
+        longitude: -49.226749,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }}
+    />
   );
 }
 
