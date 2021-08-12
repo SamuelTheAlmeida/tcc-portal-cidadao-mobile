8451430308ff98b3be1890c26ebf8ffa474c2945
Samuel T Almeida <samuel.almeida@capgemini.com>
2021-08-12 20:00:12 -0300

    markers fake

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,11 +1,56 @@
+import axios from 'axios';
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import RNFetchBlob from 'rn-fetch-blob';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+interface Postagem {
+  id: number;
+  latitude?: number;
+  longitude?: number;
+}
 
 export default function TabOneScreen() {
+  const [posts, setPosts] = useState(null);
+  const state = {
+    markers: [{
+      title: 'hello',
+      coordinates: {
+        latitude: -25.412127,
+        longitude: -49.226749
+      },
+    },
+    {
+      title: 'hello',
+      coordinates: {
+        latitude: -25.4300759,
+        longitude: -49.2717015
+      },  
+    }]
+  }
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "http://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      httpsAgent: {  
+        rejectUnauthorized: false
+      }
+    })
+      .then((response) => {
+        console.log(response.data);
+        setPosts(response.data.dados);
+      })
+      .catch((error) => {
+        console.log('teste:' + error);
+      });
+  }, []); // "[]" makes sure the effect will run only once.
+  
   return (
    /*<View style={styles.container}>
       <Text style={styles.title}>Portal Cidadão</Text>
@@ -13,6 +58,8 @@ export default function TabOneScreen() {
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>-->*/
     <MapView
+      showsPointsOfInterest = {false}
+      customMapStyle = {customMapStyles}
       style={{
         flex: 1
       }}
@@ -21,8 +68,20 @@ export default function TabOneScreen() {
         longitude: -49.226749,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421
-      }}
-    />
+      }}>
+
+      {state.markers.map((marker, index) => {
+          return (<Marker
+          key={index}
+          coordinate={marker.coordinates}
+          title={'hello'}
+          description={'test'}
+        >
+
+        </Marker>)
+      })}
+
+      </MapView>
   );
 }
 
@@ -42,3 +101,40 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
+const customMapStyles = [
+  {
+    featureType: "administrative",
+    elementType: "geometry",
+    stylers: [
+    {
+        visibility: "off"
+    }
+    ]
+  },
+  {
+    featureType: "poi",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "road",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "transit",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  }
+];
